80b8d94ade7b0d809d10c24530c30f5bc7d22cf1
miguel aguila <A01706165@itesm.mx>
2021-03-19 00:58:27 -0600

    Mejora 1

--- cancion_final.py
+++ cancion_final.py
@@ -1,17 +1,17 @@
+# Ejemplo con la duración 
 from music21 import note, stream
 
-#notes = ['E','D#','E','B3','D','C','A3'] # Nombre de las notas
-notes = ['E','D#','E','B3','D','C'] # Notas con misma duración
+notes = ['A3','D4','E4','D4','A3','D4','E4','D4','A3','D4','E4','D4','A3','D4','E4','D4'] # Notas con misma duración
 
 st = stream.Stream()
 
 for n in notes:
   new_note = note.Note(n) 
-  new_note.duration.quarterLength = 1/2 # Duración
+  new_note.duration.quarterLength = 1 # Duración
   st.append(new_note)
 
-new_note = note.Note('A3') # Nota con diferente duración
-new_note.duration.quarterLength = 2 # Duración
+new_note = note.Note('A2') # Nota con diferente duración
+new_note.duration.quarterLength = 16 # Duración
 st.append(new_note)
 
-st.write('midi', fp = 'cancion11.mid') 
+st.write('midi', fp = 'cancion13.mid')
