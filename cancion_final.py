ff30516a7f9439570017a7c2f95234504dd8a4c0
miguel aguila <A01706165@itesm.mx>
2021-03-19 08:39:58 -0600

    Grupo 4 d enotas con inicio de notas individuales

--- cancion_final.py
+++ cancion_final.py
@@ -1,5 +1,5 @@
 # Ejemplo con la duración 
-from music21 import note, stream, chord
+from music21 import note, stream
 
 st = stream.Stream()
 
@@ -33,4 +33,14 @@ for n3 in notes3:
   new_note.duration.quarterLength = 1/2 # Duración
   st.append(new_note)
 
-st.write('midi', fp = 'cancion30.mid')
+notes4 = ['A3','D4','F4#','D4','A3','D4','F4#','D4','A3','D4','F4#','D4','A3','D4','F4#','D4']
+
+for n4 in notes4:
+  new_note1 = note.Note('D3') # Nota con diferente duración
+  new_note1.duration.quarterLength = 16 # Duración 
+  new_note1.offset = 48
+  new_note = note.Note(n4) 
+  new_note.duration.quarterLength = 1/2 # Duración
+  st.append(new_note)
+
+st.write('midi', fp = 'cancion31.mid')
