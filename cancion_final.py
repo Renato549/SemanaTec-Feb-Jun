69b1134d2a7a0397a542e7cd768b8ada98f52fac
miguel aguila <A01706165@itesm.mx>
2021-03-19 08:05:21 -0600

    Intento de acorde no.27

--- cancion_final.py
+++ cancion_final.py
@@ -1,17 +1,26 @@
 # Ejemplo con la duración 
-from music21 import note, stream
-
-notes = ['A3','D4','E4','D4','A3','D4','E4','D4','A3','D4','E4','D4','A3','D4','E4','D4'] # Notas con misma duración
+from music21 import note, stream, chord
 
 st = stream.Stream()
 
+notes = ['A3','D4','E4','D4','A3','D4','E4','D4','A3','D4','E4','D4','A3','D4','E4','D4'] # Notas con misma duración
+
 for n in notes:
+  new_note1 = note.Note('A2') # Nota con diferente duración
+  new_note1.duration.quarterLength = 16 # Duración
+  new_note1.offset = 0 
   new_note = note.Note(n) 
-  new_note.duration.quarterLength = 1 # Duración
+  new_note.duration.quarterLength = 1/2 # Duración
   st.append(new_note)
 
-new_note = note.Note('A2') # Nota con diferente duración
-new_note.duration.quarterLength = 16 # Duración
-st.append(new_note)
+notes2 = ['A2','D4','E4','D4','A2','D4','E4','D4','A2','D4','E4','D4','A2','D4','E4','D4'] # Notas con misma duración
 
-st.write('midi', fp = 'cancion13.mid')
+for n2 in notes2:
+  new_note1 = note.Note('D3') # Nota con diferente duración
+  new_note1.duration.quarterLength = 16 # Duración 
+  new_note1.offset = 16
+  new_note = note.Note(n2) 
+  new_note.duration.quarterLength = 1/2 # Duración
+  st.append(new_note)
+
+st.write('midi', fp = 'cancion27.mid')
